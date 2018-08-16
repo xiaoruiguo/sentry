3cf2d546e764ea988fd0483e4aa9f4aba9ce2d51
Billy Vong <billy@sentry.io>
2018-08-15 18:25:11 -0700

    feat(health): HealthRequest render prop should be passed component props
    Sometimes you may want to access props passed to HealthRequest in your render prop

--- src/sentry/static/sentry/app/views/organizationHealth/util/healthRequest.jsx
+++ src/sentry/static/sentry/app/views/organizationHealth/util/healthRequest.jsx
@@ -152,7 +152,7 @@ class HealthRequestWithParams extends React.Component {
   };
 
   render() {
-    let {children} = this.props;
+    let {children, ...props} = this.props;
     let {data} = this.state;
 
     return children({
@@ -160,6 +160,9 @@ class HealthRequestWithParams extends React.Component {
       loading: data === null,
       data: this.transformData(data),
       originalData: data,
+
+      // sometimes we want to reference props that was given to HealthRequest
+      ...props,
     });
   }
 }
